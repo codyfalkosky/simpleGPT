ef6b56bc18a5af87bf9e4816082f87e9a952e1c2
Cody Falkosky <codyfalkosky@gmail.com>
2024-05-07 14:25:32 -0700

    removed import matplotlib

--- simpleGPT.py
+++ simpleGPT.py
@@ -3,10 +3,8 @@ from tensorflow.keras.layers import Dense, LayerNormalization, Softmax, Embeddin
 from tensorflow.keras.losses import sparse_categorical_crossentropy
 from tensorflow.keras.metrics import Mean
 from .custom_layers import GPTModel
-# from IPython.display import clear_output
 # import tiktoken
 import sentencepiece as spm
-import matplotlib.pyplot as plt
 import datetime
 import time
 import os
